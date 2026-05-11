7a4401a16afe51570917382e3e8927238129c2ba
SajjadHossaain <sajjad.iiucian@gmail.com>
2026-05-12 00:49:57 +0600

    login page give functionality

--- script/login.js
+++ script/login.js
@@ -1,1 +1,23 @@
 // console.log("Login function connected")
+document.getElementById("login-btn").addEventListener("click",function(){
+    // console.log("Login Button CLick");
+
+    //1-get the mobile number input
+    const numberInput = document.getElementById("input-number");
+    const contactNumber = numberInput.value;
+    // console.log(contactNumber);
+    //2-get the pin input
+    const inputPin = document.getElementById("input-pin");
+    const pin =inputPin.value;
+    // console.log(pin);
+    //3-match pin and mobile number
+    if(contactNumber == "01234567890" && pin == "1234"){
+        //3-1 true ::: >> alert > homepage
+        alert("Login Success");
+    }
+    else{
+        //3-1 false ::: >> alert > return
+        alert("Login Failed");
+        return;
+    }
+});
